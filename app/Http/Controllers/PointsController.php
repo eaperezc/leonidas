d705dd39d9f399513559f26ddbc52a96d45e8c02
Enrique Perez <eperez@sproutloud.com>
2016-12-23 16:26:40 -0500

    testing request 2

--- app/Http/Controllers/PointsController.php
+++ app/Http/Controllers/PointsController.php
@@ -20,7 +20,7 @@ class PointsController extends Controller
 
     public function balance(Request $request)
     {
-        return $request;
+        return $request->input('account_id');
 
         $balance_response = [];
         if (isset($this->request['account_id'])) {
