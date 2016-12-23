968de2db24924b598637dee0e2ca927e837ebbd4
Enrique Perez <eperez@sproutloud.com>
2016-12-23 10:33:02 -0500

    balance route

---
+++ app/Http/Controllers/PointsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PointsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function balance()
+    {
+        return 100;
+    }
+}
